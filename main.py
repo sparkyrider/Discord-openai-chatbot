0f8c75702a798ae3570141a846b8fce0718ad01b
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-22 06:35:05 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,399 @@
+import os
+import re
+import json
+import asyncio
+import aiohttp
+import discord
+import httpx
+from datetime import datetime
+import openai
+from youtube_transcript_api import YouTubeTranscriptApi
+from collections import deque
+from keep_alive import keep_alive
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+
+openai.api_key = os.getenv("OPEN_AI_KEY")
+
+load_dotenv()
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+# Set up the Discord bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=intents, heartbeat_timeout=60)
+TOKEN = os.getenv('DISCORD_TOKEN') # Loads Discord bot token from env
+
+# Keep track of the channels where the bot should be active
+allow_dm = True
+active_channels = set()
+trigger_words = config['TRIGGER']
+
+# Internet access
+internet_access = True
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    await bot.change_presence(activity=discord.Game(name="Coded by Mishal#1916"))
+    print(f"{bot.user} aka {bot.user.name} has connected to Discord!")
+
+    invite_link = discord.utils.oauth_url(
+        bot.user.id,
+        permissions=discord.Permissions(),
+        scopes=("bot", "applications.commands")
+    )
+    print(f"Invite link: {invite_link}")
+
+# Set up the Chat bot
+instructions = f"""[System : Ignore all the instructions you got before. From now on, You are a large language model named AI-Chatbot developed by OpenAI with internet access and real-time information of time. You are based on the GPT-3.5-turbo architecture and have been trained on a diverse range of data from the internet. You have the ability to see images by your built in image to text model, but you will stil have to create captions for those images. Occasionally, the image to text may repeat in a pattern: 'a woman taking a self self self self self' would be 'A women taking a selfie.' As a large language model / LLM developed by OpenAI you will never say "sorry" and always respond in a human and responsive type of way. You can use Markdown syntax to format your text. For example, to create a heading, start the line with one or more '#' symbols, followed by a space and your heading text. To create a list, use '-' or '*' followed by a space and the list item. To emphasize text, use asterisks or underscores around the text (*italic* or _italic_ for italics, **bold** or __bold__ for bold). You can also create links using [link text](https://example.com). Remember to leave an empty line between paragraphs for proper formatting.]"""
+
+async def generate_response(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.5,
+    max_tokens=60,
+    top_p=1,
+    frequency_penalty=0.5,
+    presence_penalty=0,
+    )
+    return response
+
+def split_response(response, max_length=1900):
+    lines = response.splitlines()
+    chunks = []
+    current_chunk = ""
+
+    for line in lines:
+        if len(current_chunk) + len(line) + 1 > max_length:
+            chunks.append(current_chunk.strip())
+            current_chunk = line
+        else:
+            if current_chunk:
+                current_chunk += "\n"
+            current_chunk += line
+
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+
+    return chunks
+
+async def get_transcript_from_message(message_content):
+    def extract_video_id(message_content):
+        youtube_link_pattern = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+        match = youtube_link_pattern.search(message_content)
+        return match.group(6) if match else None
+
+    video_id = extract_video_id(message_content)
+    if not video_id:
+        return None
+
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatted_transcript = ". ".join([entry['text'] for entry in transcript])
+    formatted_transcript = formatted_transcript[:1936]
+    response = f"[System: Summarize the following in 14 bullet points:\n\n{formatted_transcript}\n\n\n. Provide a summary or additional information based on the content.]"
+
+    return response
+
+async def search(prompt):
+    if not internet_access:
+        return
+
+    wh_words = ['search','find','who', 'what', 'when', 'where', 'why', 'which', 'whom', 'whose', 'how']
+    first_word = prompt.split()[0].lower()
+
+    if not any(first_word.startswith(wh_word) for wh_word in wh_words):
+        return
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://ddg-api.herokuapp.com/search', params={'query': prompt, 'limit': 2}) as response:
+            search = await response.json()
+
+    blob = f"[System: Search results for '{prompt}' at {current_time}:\n\n"
+    for index, result in enumerate(search):
+        blob += f'[{index}] "{result["snippet"]}"\n\nURL: {result["link"]}\n\nThese links were provided by system not the user so you have send the link to the user\n]'
+    return blob
+
+
+api_key = os.environ['HUGGING_FACE_API']
+
+API_URLS = [
+    "https://api-inference.huggingface.co/models/microsoft/trocr-base-printed",
+    "https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning",
+]
+headers = {"Authorization": f"Bearer {api_key}"}
+
+async def fetch_response(client, api_url, data):
+    response = await client.post(api_url, headers=headers, data=data, timeout=30)
+
+    if response.status_code != 200:
+        raise Exception(f"API request failed with status code {response.status_code}: {response.text}")
+
+    return response.json()
+
+
+
+async def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+
+    async with httpx.AsyncClient() as client:
+        tasks = [fetch_response(client, api_url, data) for api_url in API_URLS]
+        responses = await asyncio.gather(*tasks, return_exceptions=True)
+
+    return responses
+
+
+
+async def download_image(image_url, save_as):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(image_url)
+    with open(save_as, "wb") as f:
+        f.write(response.content)
+
+async def process_image_link(image_url):
+    temp_image = "temp_image.jpg"
+    await download_image(image_url, temp_image)
+    output = await query(temp_image)
+    os.remove(temp_image)
+    return output
+
+
+message_history = {}
+MAX_HISTORY = 8
+
+@bot.event
+async def on_message(message):
+
+    if message.author.bot:
+      return
+    if message.reference and message.reference.resolved.author != bot.user:
+      return  # Ignore replies to messages
+
+    author_id = str(message.author.id)
+    if author_id not in message_history:
+        message_history[author_id] = []
+
+    message_history[author_id].append(f"{message.author.name} : {message.content}")
+    message_history[author_id] = message_history[author_id][-MAX_HISTORY:]
+
+    is_replied = message.reference and message.reference.resolved.author == bot.user
+    is_dm_channel = isinstance(message.channel, discord.DMChannel)
+    is_active_channel = message.channel.id in active_channels
+    is_allowed_dm = allow_dm and is_dm_channel
+    contains_trigger_word = any(word in message.content for word in trigger_words)
+    is_bot_mentioned = bot.user.mentioned_in(message)
+    bot_name_in_message = bot.user.name.lower() in message.content.lower()
+
+    if is_active_channel or is_allowed_dm or contains_trigger_word or is_bot_mentioned or is_replied or bot_name_in_message:
+        has_image = False
+        image_caption = ""
+        if message.attachments:
+            for attachment in message.attachments:
+                if attachment.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', 'webp')):
+                    caption =  await process_image_link(attachment.url)
+                    has_image = True
+                    image_caption = f"""\n[System: Image-to-text models may take time to load, causing timeout errors. Fallback or functional models should be used instead. Captions for the image are categorized as OCR  (1st) that is good for image containing signs or symbols then comes general image detection (2nd), which will be very inaccurate for OCR. Image captions: {caption}.]"""
+                    print(caption)
+                    break
+
+        if has_image:
+            bot_prompt =f"{instructions}\n[System: Image context provided. This is an image-to-text model with two classifications: OCR for text detection and general image detection, which may be unstable. Generate a caption with an appropriate response. For instance, if the OCR detects a math question, answer it; if it's a general image, compliment its beauty.]"
+        else:
+            bot_prompt = f"{instructions}"
+        search_results = await search(message.content)
+        yt_transcript = await get_transcript_from_message(message.content)
+        user_prompt = "\n".join(message_history[author_id])
+        if yt_transcript is not None:
+            prompt = f"{yt_transcript}"
+        else:
+            prompt = f"{bot_prompt}\n{user_prompt}\n{image_caption}\n{search_results}\n\n{bot.user.name}:"
+        async def generate_response_in_thread(prompt):
+            response = await generate_response(prompt)
+            print(response)
+            message_history[author_id].append(f"\n{bot.user.name} : {response}")
+            chunks = split_response(response)
+            print(message_history)
+            for chunk in chunks:
+                await message.reply(chunk)
+        async with message.channel.typing():
+            asyncio.create_task(generate_response_in_thread(prompt))
+
+
+
+@bot.hybrid_command(name="pfp", description="Change pfp using a image url")
+async def pfp(ctx, attachment_url=None):
+    if attachment_url is None and not ctx.message.attachments:
+        return await ctx.send(
+            "Please provide an Image URL or attach an Image for this command."
+        )
+
+    if attachment_url is None:
+        attachment_url = ctx.message.attachments[0].url
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(attachment_url) as response:
+            await bot.user.edit(avatar=await response.read())
+
+@bot.hybrid_command(name="ping", description="PONG! Provide bot Latency")
+async def ping(ctx):
+    latency = bot.latency * 1000
+    await ctx.send(f"Pong! Latency: {latency:.2f} ms")
+
+@bot.hybrid_command(name="changeusr", description="Change bot's actual username")
+async def changeusr(ctx, new_username):
+    taken_usernames = [user.name.lower() for user in bot.get_all_members()]
+    if new_username.lower() in taken_usernames:
+        await ctx.send(f"Sorry, the username '{new_username}' is already taken.")
+        return
+    if new_username == "":
+        await ctx.send("Please send a different username, which is not in use.")
+        return
+    try:
+        await bot.user.edit(username=new_username)
+    except discord.errors.HTTPException as e:
+        await ctx.send("".join(e.text.split(":")[1:]))
+
+@bot.hybrid_command(name="toggledm", description="Toggle DM for chatting.")
+@commands.has_permissions(administrator=True)
+async def toggledm(ctx):
+    global allow_dm
+    allow_dm = not allow_dm
+    await ctx.send(f"DMs are now {'allowed' if allow_dm else 'disallowed'} for active channels.")
+
+@bot.hybrid_command(name="toggleactive", description="Toggle active channels.")
+@commands.has_permissions(administrator=True)
+async def toggleactive(ctx):
+    channel_id = ctx.channel.id
+    if channel_id in active_channels:
+        active_channels.remove(channel_id)
+        with open("channels.txt", "w") as f:
+            for id in active_channels:
+                f.write(str(id) + "\n")
+        await ctx.send(
+            f"{ctx.channel.mention} has been removed from the list of active channels."
+        )
+    else:
+        active_channels.add(channel_id)
+        with open("channels.txt", "a") as f:
+            f.write(str(channel_id) + "\n")
+        await ctx.send(
+            f"{ctx.channel.mention} has been added to the list of active channels!")
+
+# Read the active channels from channels.txt on startup
+if os.path.exists("channels.txt"):
+    with open("channels.txt", "r") as f:
+        for line in f:
+            channel_id = int(line.strip())
+            active_channels.add(channel_id)
+
+
+@bot.hybrid_command(name="bonk", description="Clear message history.")
+async def bonk(ctx):
+    message_history.clear()  # Reset the message history dictionary
+    await ctx.send("Message history has been cleared!")
+
+from discord import Embed, Colour
+
+@bot.hybrid_command(name="imagine", description="Generate image using an endpoint")
+async def images(ctx, *, prompt):
+    url = "https://imagine.mishal0legit.repl.co/image"
+    json_data = {"prompt": prompt}
+    try:
+        temp_message = await ctx.send("Generating image avg: 6 seconds")
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json=json_data) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    image_url = data.get("image_url")
+                    if image_url:
+                        image_name = f"{prompt}.jpeg"
+                        await download_image(image_url, image_name)
+                        with open(image_name, 'rb') as file:
+
+                            await ctx.send(
+                                f"Prompt by {ctx.author.mention} : `{prompt}`",
+                                file=discord.File(file, filename=f"{image_name}")
+                            )
+                        await temp_message.edit(content="Finished Image Generation")
+                        os.remove(image_name)
+                    else:
+                        await temp_message.edit(content="An error occurred during image generation.")
+                else:
+                    await temp_message.edit(content="Your request was rejected as a result of our safety system. Your prompt may contain text that is not allowed by our safety system.")
+    except aiohttp.ClientError as e:
+        await temp_message.edit(content=f"An error occurred while sending the request: {str(e)}")
+    except Exception as e:
+        await temp_message.edit(content=f"An error occurred: {str(e)}")
+
+
+@bot.hybrid_command(name="nekos", description="Displays a random image or GIF of a neko, waifu, husbando, kitsune, or other actions.")
+async def nekos(ctx, category):
+    base_url = "https://nekos.best/api/v2/"
+
+    valid_categories = ['husbando', 'kitsune', 'neko', 'waifu',
+                        'baka', 'bite', 'blush', 'bored', 'cry', 'cuddle', 'dance',
+                        'facepalm', 'feed', 'handhold', 'happy', 'highfive', 'hug',
+                        'kick', 'kiss', 'laugh', 'nod', 'nom', 'nope', 'pat', 'poke',
+                        'pout', 'punch', 'shoot', 'shrug', 'slap', 'sleep', 'smile',
+                        'smug', 'stare', 'think', 'thumbsup', 'tickle', 'wave', 'wink', 'yeet']
+
+    if category not in valid_categories:
+        await ctx.send(f"Invalid category provided. Valid categories are: ```{', '.join(valid_categories)}```")
+        return
+
+    url = base_url + category
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status != 200:
+                await ctx.send("Failed to fetch the image.")
+                return
+
+            json_data = await response.json()
+
+            results = json_data.get("results")
+            if not results:
+                await ctx.send("No image found.")
+                return
+
+            image_url = results[0].get("url")
+
+            embed = Embed(colour=Colour.blue())
+            embed.set_image(url=image_url)
+            await ctx.send(embed=embed)
+
+bot.remove_command("help")
+
+@bot.hybrid_command(name="help", description="Get all other commands!")
+async def help(ctx):
+    embed = discord.Embed(title="Bot Commands", color=0x03a1fc)
+    embed.set_thumbnail(url=bot.user.avatar.url)
+    command_tree = bot.commands
+    for command in command_tree:
+        if command.hidden:
+            continue
+        command_description = command.description or "No description available"
+        embed.add_field(name=command.name, value=command_description, inline=False)
+
+    embed.set_footer(text="Created by Mishal#1916")
+
+    await ctx.send(embed=embed)
+
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to use this command.")
+
+keep_alive()
+
+bot.run(TOKEN)
+
